2400428cb1b601e14e4c636ff7d0cfc66158f6da
Ezio Maina <ezio.maina@unito.it>
2021-09-23 11:11:37 +0200

    Initial commit

---
+++ ipynb/pippo.py
@@ -0,0 +1,10 @@
+print("Questo è il file pippo.py\n")
+print("Esegue somma e prodotto di due numeri reali fissati.\n")
+
+a = 1.3
+b = 5.7
+somma = a+b
+prodotto = a*b
+
+print("La somma di ", a, " e ", b, " è ", somma,"\n")
+print("Il prodotto di ", a, " e ", b, " è ", prodotto,"\n")
